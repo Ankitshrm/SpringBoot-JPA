40cd2086a781b938942579b09369551ae9bfcaa5
Ankit Sharma <ankitsharma97194@gmail.com>
2023-01-28 20:15:08 +0530

    Springboot JPA

--- src/main/java/com/springboot/jpa/JpaApplication.java
+++ src/main/java/com/springboot/jpa/JpaApplication.java
@@ -1,6 +1,6 @@
 package com.springboot.jpa;
 
-import java.util.Iterator;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -23,60 +23,70 @@ public class JpaApplication {
 		
 		//for single instance 
 		
-		User user =new User();
-		user.setCity("GOA");
-		user.setName("Ankit Sharma");
-		User u= userRepository.save(user);
-		System.out.println(u);
+//		User user =new User();
+//		user.setCity("GOA");
+//		user.setName("Ankit Sharma");
+//		User u= userRepository.save(user);
+//		System.out.println(u);
+////		
+////		//for multiple instance
 //		
-//		//for multiple instance
-		
-		User user2=new User();
-		user2.setCity("DOON");
-		user2.setName("Aman");
-		
-		User user3=new User();
-		user3.setCity("J&K");
-		user3.setName("Bali");
-		
-		List<User> users =List.of(user2,user3);
-		Iterable<User> i =userRepository.saveAll(users);
-		
-		i.forEach(it ->{
-			System.out.println(it);
-		});
-		
-		
-		//UPDATE
-		
-//		update the user data using its id
-//		3 | J&K  | Bali
+//		User user2=new User();
+//		user2.setCity("DOON");
+//		user2.setName("Aman");
 //		
-		 Optional<User> optional = userRepository.findById(3);
-		 User userUpdate =optional.get();
-		 userUpdate.setName("Urvashi bali");
-		 userRepository.save(userUpdate);
-//		 
-//			3 | J&K  | Urvashi Bali
-
+//		User user3=new User();
+//		user3.setCity("J&K");
+//		user3.setName("Bali");
+//		
+//		List<User> users =List.of(user2,user3);
+//		Iterable<User> i =userRepository.saveAll(users);
+//		
+//		i.forEach(it ->{
+//			System.out.println(it);
+//		});
+//		
+//		
+//		//UPDATE
+//		
+////		update the user data using its id
+////		3 | J&K  | Bali
+////		
+//		 Optional<User> optional = userRepository.findById(3);
+//		 User userUpdate =optional.get();
+//		 userUpdate.setName("Urvashi bali");
+//		 userRepository.save(userUpdate);
+////		 
+////			3 | J&K  | Urvashi Bali
+//
+//		
+//		//READ 
+//		
+////		1. findById(int id) --> returning optional containing data
+////		2. findAll()
+//		Iterable<User> itr=userRepository.findAll();
+//		itr.forEach(userRead->System.out.println(userRead));
+//		
+//		
+//		//DELETE
+//		
+////		1. deletById(int id);
+//		
+//		userRepository.deleteById(3);
+//		
+////		2.deleteAll();
+//		
+//		userRepository.deleteAll();
+//		
 		
-		//READ 
+		List<User> username =userRepository.findByName("Bali");
+		username.forEach(e->System.out.println(e));
 		
-//		1. findById(int id) --> returning optional containing data
-//		2. findAll()
-		Iterable<User> itr=userRepository.findAll();
-		itr.forEach(userRead->System.out.println(userRead));
+		List<User> alluser=userRepository.getAllUsers();
+		alluser.forEach(e->System.out.println(e));
 		
-		
-		//DELETE
-		
-//		1. deletById(int id);
-		
-		userRepository.deleteById(3);
-		
-//		2.deleteAll();
-		
-		userRepository.deleteAll();
+		List<User> ans =userRepository.findByNameOrCity("Bali","DOON");
+		ans.forEach(e->System.out.println(e));
 		
 	}
 
--- src/main/java/com/springboot/jpa/dao/UserRepository.java
+++ src/main/java/com/springboot/jpa/dao/UserRepository.java
@@ -1,9 +1,20 @@
 package com.springboot.jpa.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.springboot.jpa.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	public List<User> findByName(String name);
+	
+	@Query("select u from User u")
+	public List<User> getAllUsers();
+	
+	@Query("Select u from User u where u.name=:n or u.city=:c")
+	public List<User> findByNameOrCity(@Param("n")String name,@Param("c")String city);
 }
